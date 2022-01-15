c649776d747bce88335ac446eaf1815c1799d89b
Igor Lygin (netbook) <lazycoast102@gmail.com>
2022-01-16 00:35:30 +0300

    Task 2

--- kaggle/python/LoopsListComprehensions.py
+++ kaggle/python/LoopsListComprehensions.py
@@ -13,3 +13,19 @@ def has_lucky_number(nums):
 
 print (has_lucky_number([0, -1, 7]))
 print (has_lucky_number([]))
+
+#2
+#R and Python have some libraries (like numpy and pandas) 
+# compare each element of the list to 2 (i.e. do an 'element-wise' comparison) and give us a list of 
+# booleans like `[False, False, True, True]`. 
+
+#Implement a function that reproduces this behaviour, returning a 
+# list of booleans corresponding to whether the corresponding element is greater than n.
+
+
+#def elementwise_greater_than(L, thresh):
+#   """Return a list with the same length as L, where the value at index i is 
+#   True if L[i] is greater than thresh, and False otherwise.
+    
+#   >>> elementwise_greater_than([1, 2, 3, 4], 2)
+#   [False, False, True, True]
