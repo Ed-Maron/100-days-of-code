7b6a13eb2f0d5b3508d4e99c6f74197f8517a79e
Igor Lygin (netbook) <lazycoast102@gmail.com>
2022-01-16 00:27:53 +0300

    Task 1

--- kaggle/python/LoopsListComprehensions.py
+++ kaggle/python/LoopsListComprehensions.py
@@ -1,2 +1,13 @@
-#First local test
-print("Hello world!")
+#1 
+# Have you ever felt debugging involved a bit of luck? The following program has a bug. 
+# Try to identify the bug and fix it.
+
+def has_lucky_number(nums):
+    """Return whether the given list of numbers is lucky. A lucky list contains
+    at least one number divisible by 7.
+    """
+    for num in nums:
+        if num % 7 == 0:
+            return True
+        else:
+            return False
