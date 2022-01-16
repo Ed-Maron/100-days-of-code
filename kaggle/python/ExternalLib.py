96aa08b6cce2033b27510e49bb5228f425e4ec2a
Igor Lygin (netbook) <lazycoast102@gmail.com>
2022-01-16 15:30:32 +0300

    closed  task 2

--- kaggle/python/ExternalLib.py
+++ kaggle/python/ExternalLib.py
@@ -9,11 +9,11 @@ def best_items(racers):
         racer = racers[i]
         # We're only interested in racers who finished in first
         if racer['finish'] == 1:
-            for i in racer['items']:
+            for item in racer['items']:
                 # Add one to the count for this item (adding it to the dict if necessary)
-                if i not in winner_item_counts:
-                    winner_item_counts[i] = 0
-                winner_item_counts[i] += 1
+                if item not in winner_item_counts:
+                    winner_item_counts[item] = 0
+                winner_item_counts[item] += 1
 
         # Data quality issues :/ Print a warning about racers with no name set. We'll take care of it later.
         if racer['name'] is None:
