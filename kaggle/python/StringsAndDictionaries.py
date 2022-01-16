ba90e982149b5254902a2dcfa407654040f683f1
Igor Lygin (netbook) <lazycoast102@gmail.com>
2022-01-16 13:56:23 +0300

    task2

--- kaggle/python/StringsAndDictionaries.py
+++ kaggle/python/StringsAndDictionaries.py
@@ -15,3 +15,26 @@ def is_valid_zip(zip_code):
 #    ,is_valid_zip('/n')])
 
 
+
+#2
+#A researcher has gathered thousands of news articles. But she wants to focus her attention on articles including a specific word. Complete the function below to help her filter her list of articles.
+
+#Your function should meet the following criteria:
+
+#Do not include documents where the keyword string shows up only as a part of a larger word. For example, if she were looking for the keyword “closed”, you would not include the string “enclosed.”
+#She does not want you to distinguish upper case from lower case letters. So the phrase “Closed the case.” would be included when the keyword is “closed”
+#Do not let periods or commas affect what is matched. “It is closed.” would be included when the keyword is “closed”. But you can assume there are no other types of punctuation.
+
+def word_search(doc_list, keyword):
+    i=0
+    result = []
+    for i, row in enumerate(doc_list):  
+        words = row.split() 
+        formated_words = [el.strip('.,').lower() for el in words]
+        if keyword.lower() in formated_words:
+            result.append(i)
+        i+=1
+    return result
+
+#doc_list = ["The Learn Python Challenge Casino.", "They bought a car", "Casinoville"]
+#print(word_search(doc_list, 'casino'))
