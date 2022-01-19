8582eb0be5e84d3ff0e08cbae75f7977467db08a
Igor Lygin (netbook) <lazycoast102@gmail.com>
2022-01-20 02:39:49 +0300

    test API

--- openweathermap api/main.py
+++ openweathermap api/main.py
@@ -3,10 +3,32 @@ from  requests import get
 from city_weather import City 
 
 cities = ["Petersburg","Moscow"]
-appid = "3e6f7c567fcc226aa6b7ab089fd6380e"
+appid = ""
 
 cities_id = dict()
 
-a = City('Moscow')
-pass
+for city in cities:
+    try:
+        res = get("http://api.openweathermap.org/data/2.5/find",
+                    params={'q':city, 'units': 'metric', 'APPID': appid})
+        data = res.json()
+        cities_id[data['list'][0]['name']]=data['list'][0]['id']
+    except Exception as e:
+        print("Exception (find):", e)
 
+result = dict()
+for key in cities_id:
+    try:
+        res = get("http://api.openweathermap.org/data/2.5/weather",
+                    params={'id':cities_id[key], 'units': 'metric', 'APPID': appid})
+        data = res.json()
+        result[key]=data
+    except Exception as e:
+        print("Exception (find):", e)
+        
+print(result['Moscow'])
+
+res = get("http://api.openweathermap.org/data/2.5/onecall/timemachine",
+                    params={'lat':'60.99', 'lon': '30.9', 'APPID': appid, 'dt':'1642275329'})
+data = res.json()
+print(data)
