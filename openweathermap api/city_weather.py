c614d37b317ab3384fb435cb0ab3ce3c09bd75c2
Igor Lygin (netbook) <lazycoast102@gmail.com>
2022-01-20 02:38:11 +0300

    Add new class weather

--- openweathermap api/city_weather.py
+++ openweathermap api/city_weather.py
@@ -1,20 +1,51 @@
+from datetime import datetime
+
 class City:
 
     def __init__(self, name):
         self.__name = name
-        self.__id = None
-    
+
     @property
     def name(self):
         return self.__name
 
     @property
-    def id(self):
-        return self.__id
+    def weather_data(self):
+        return self.__weather_data
 
-    @id.setter
-    def id(self, id):
-        if  id == -1:
-            self.__id = 0
-        else:
-            self.__id = id
+    @weather_data.setter
+    def weather_data(self, data):
+        result = set({Weather(el) for el in data})
+        pass
+
+
+
+class Weather:
+
+    def __init__(self, info):
+        self.__date_log = datetime.today()
+        self.__temp = 0
+        self.__feels_like  = 0
+        self.__humidity = 0 
+        self.__visibility = 0
+ 
+    #getters       
+    @property
+    def temp(self):
+        return self.__temp
+
+    @property
+    def date_log(self):
+        return self.__date_log
+
+    @property
+    def feels_like(self):
+        return self.__feels_like   
+    
+    @property
+    def humidity(self):
+        return self.__humidity
+
+    @property
+    def visibility(self):
+        return self.__visibility
--- openweathermap api/main.py
+++ openweathermap api/main.py
@@ -7,23 +7,6 @@ appid = "3e6f7c567fcc226aa6b7ab089fd6380e"
 
 cities_id = dict()
 
-for city in cities:
-    try:
-        res = get("http://api.openweathermap.org/data/2.5/find",
-                    params={'q':city, 'units': 'metric', 'APPID': appid})
-        data = res.json()
-        cities_id[data['list'][0]['name']]=data['list'][0]['id']
-    except Exception as e:
-        print("Exception (find):", e)
+a = City('Moscow')
+pass
 
-result = dict()
-for key in cities_id:
-    try:
-        res = get("http://api.openweathermap.org/data/2.5/weather",
-                    params={'id':cities_id[key], 'units': 'metric', 'APPID': appid})
-        data = res.json()
-        result[key]=data
-    except Exception as e:
-        print("Exception (find):", e)
-        
-print(result)
