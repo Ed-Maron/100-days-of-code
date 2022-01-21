4e418a5676a0897a0f9cb83d01a55ee1c270e302
Igor Lygin (netbook) <lazycoast102@gmail.com>
2022-01-22 01:31:11 +0300

    import  matplotlib

--- openweathermap api/main.py
+++ openweathermap api/main.py
@@ -2,8 +2,13 @@ from asyncio.windows_events import NULL
 from unicodedata import name
 from  requests import get 
 from city import City 
+import matplotlib.pyplot as plt
+
 
 cities = ["Petersburg","Moscow"]
 
-result = [City(name) for name in cities]
+#result = [City(name) for name in cities]
+
+plt.plot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+plt.show()
 pass
